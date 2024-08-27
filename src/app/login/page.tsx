4778727a3a11f7d4f6ff6353a19134187ded2946
Evelyn Ernst <evelyn.ernst@uap.edu.ar>
2024-08-27 11:53:27 -0300

    Initial commit

---
+++ src/app/login/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+export default function login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <form action="/login" method="post">
+        <input type="text" name="username" placeholder="Username" />
+        <input type="password" name="password" placeholder="Password" />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
